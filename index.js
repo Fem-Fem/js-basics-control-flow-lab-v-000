71defc7f84fd327e1737cbf11d50a1b8fc48faae
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-27 02:18:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ function scuberGreetingForFeet (feet) {
   }
 };
 
-function ternaryCheckCit (feet) {
-  if (feet <= 400) {
-    return "This one is on me!"
-  }
-  else if (feet >= 2500) {
-    return 'No can do.'
-  }
-  else if (feet >= 2000) {
-    return 'I will gladly take your thirty bucks.'
-  }
+function ternaryCheckCity (city) {
+  city == "NYC" ? return 'Ok, sounds good.' : return "No go."
 };
