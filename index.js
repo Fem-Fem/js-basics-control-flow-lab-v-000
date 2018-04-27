4e937f179ade20c28f3ea3e57eb4855fac7b9f4b
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-27 02:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
     "This one is on me!"
   }
+  else if (feet >= 2500) {
+    'No can do.'
+  }
   else if (feet >= 2000) {
     'I will gladly take your thirty bucks.'
   }
